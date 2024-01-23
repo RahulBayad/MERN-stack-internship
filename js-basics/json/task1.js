b87129a80f94304329076ba0a8622c37b702712d
RahulBayad <rahulbayad05@gmail.com>
2024-01-23 22:09:08 +0530

    json

---
+++ js-basics/json/task1.js
@@ -0,0 +1,118 @@
+// EXAM JSON
+
+let exam = {
+    examName : "Winter 2023",
+    duration : "150 minutes",
+    instructions: [
+        "Read the question before writing answer.",
+        "Use only blue pen",
+        "smart watches and smart phone not allowed"
+    ],
+    branch : "it"
+}
+
+//university
+
+let university = {
+    universityName : "GIT",
+    universityCode : "012",
+    Courses : [
+        "Information Technology",
+        "Computer Engineering",
+        "Mechanical Engineering",
+        "Civil Engineering",
+        "Electronics and Communication",
+        "Electrical Engineering"
+    ],
+    universityAddress : {
+        Location : "Khatraj-Kalol road , Moti Bhoyan",
+        city : "Gandhinagar",
+        State : "Gujarat"
+    },
+    universityContact : "079-86397730",
+    Type : "Self finance University"
+}
+
+// Employee
+
+let employee = {
+    id : "1",
+    firstName: "John",
+      lastName: "Cena",
+      position: "Software Engineer",
+      department: "Engineering",
+      salary: 80000,
+      contact: {
+        email: "john.cena@example.com",
+        phone: "+91 8833969630"
+      },
+      address: {
+        street: "123 ABC road",
+        city: "Mumbai",
+        state: "Maharashtra",
+        PinCode: "486839"
+      }
+}
+
+//student 
+
+let student = {
+    id : "1",
+    fname : "abc",
+    lname : "def",
+    mname : "xyz",
+    department : "IT",
+    contact: {
+        email: "abc@gmail.com",
+        phone: "+91 6773969630"
+      },
+      address: {
+        street: "23/664, Ab road",
+        city: "Thane",
+        state: "Maharashtra",
+        PinCode: "486674"
+      }
+}
+
+//Bank User
+
+let bankCustomer = {
+    customerId: "123456789",
+    firstName: "John",
+    lastName: "Doe",
+    dob: "1990-05-15",
+    gender: "Male",
+    contact: {
+      email: "john.doe@example.com",
+      phone: "+1 (555) 123-4567"
+    },
+    address: {
+      street: "789 Maple Ave",
+      city: "Cityville",
+      state: "CA",
+      zipCode: "98765"
+    },
+    accounts: [
+      {
+        accountId: "A12345",
+        accountType: "Savings",
+        balance: 5000.00
+      },
+      {
+        accountId: "A67890",
+        accountType: "Checking",
+        balance: 2500.00
+      }
+    ],
+    loans: [
+      {
+        loanId: "L98765",
+        loanType: "Home Loan",
+        amount: 150000.00,
+        interestRate: 4.5,
+        monthlyPayment: 1200.00,
+        remainingBalance: 120000.00
+      }
+    ]
+  };
+  
