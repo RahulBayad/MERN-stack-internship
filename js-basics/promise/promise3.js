1201e919cff33a552f148250637806ab8f740905
RahulBayad <rahulbayad05@gmail.com>
2024-01-28 21:31:11 +0530

    promise

---
+++ js-basics/promise/promise3.js
@@ -0,0 +1,32 @@
+const fetchStudents = () => {
+
+    console.log("start")
+    
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          //    resolve(["user1","user2","user3"])
+          reject({ error: "server error" });
+        }, 3000);
+      });
+    
+    console.log("end")
+    };
+    
+    // var promise = fetchStudents()
+    // console.log(promise)
+    // promise.then((students)=>{
+    //     console.log(students)
+    // }).catch((error)=>{
+    //     console.log(error)
+    // })
+    
+    console.log("start1");
+    fetchStudents()
+      .then((students) => {
+        console.log(students);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    
+      console.log("end1");
